78085791b8b20566f4b28ec78ccda41a7b3b0a72
Samuel Maciel <ssmaciel_rp@hotmail.com>
2022-12-23 17:20:19 -0300

    processamento

---
+++ IntegraCTE.Core/Entity/Destinatario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegraCTE.Core.Entity
+{
+    public class Destinatario
+    {
+        public string CNPJCPF { get; set; }
+        public string Nome { get; set; }
+        public string Logradouro { get; set; }
+        public string Nro { get; set; }
+        public string Bairro { get; set; }
+        public string CodigoMunicipio { get; set; }
+        public string Municipio { get; set; }
+        public string CEP { get; set; }
+        public string UF { get; set; }
+        public string CodigoPais { get; set; }
+        public string Pais { get; set; }
+    }
+}
--- IntegraCTE.Core/UseCases/ProcessarXMLCTE.cs
+++ IntegraCTE.Core/UseCases/ProcessarXMLCTE.cs
@@ -3,6 +3,7 @@ using IntegraCTE.Core.DTO;
 using IntegraCTE.Core.Entity;
 using IntegraCTE.Core.Model;
 using IntegraCTE.Core.Repository;
+using IntegraCTE.Core.Services;
 
 namespace IntegraCTE.Core.UseCases
 {
@@ -10,11 +11,13 @@ namespace IntegraCTE.Core.UseCases
     {
         private readonly IIntegraCTERepository _repository;
         private readonly IMapper _mapper;
+        private readonly IERPService _service;
 
-        public ProcessarXMLCTE(IIntegraCTERepository repository, IMapper mapper)
+        public ProcessarXMLCTE(IIntegraCTERepository repository, IMapper mapper, IERPService service)
         {
             _repository = repository;
             _mapper = mapper;
+            _service = service;
         }
 
         public async Task Execute(Guid id)
@@ -22,6 +25,10 @@ namespace IntegraCTE.Core.UseCases
             var arquivoModel = await _repository.BuscarArquivoCTE(id);
             var cte = _mapper.Map<CTE>(arquivoModel);
             cte.ProcessarXML();
+            var dadosNotas = await _service.BuscarDadosNotasPorChavesIN(cte.ChaveNotaFiscal);
+            var transportadoraResponse = await _service.BuscarDadosTrasnportadoraPorCNPJ(cte.Transportadora.Cnpj);
+            cte.AdicionarDadosNotas(dadosNotas);
+            cte.AdicionarDadosTransportadora(transportadoraResponse);
             var cteModel = _mapper.Map<CTEModel>(cte);
             arquivoModel.Processado = true;
             await _repository.Adicionar(cteModel);
